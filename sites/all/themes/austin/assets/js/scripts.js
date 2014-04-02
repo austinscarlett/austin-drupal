b341f9a8833ba0ddbdcfdd81d68717a6e389f5fb
Brad Sawicki <git@bradsawicki.com>
2014-04-02 14:55:48 -0500

    Form Label fade speed update

--- sites/all/themes/austin/assets/js/scripts.js
+++ sites/all/themes/austin/assets/js/scripts.js
@@ -4,23 +4,23 @@
   			
 		// Form Label Fades - Text input
 		$('.webform-client-form .form-text, .mail-list input[type="text"]').focus(function() {
-			$(this).parent().find('label:first').fadeOut();
+			$(this).parent().find('label:first').fadeOut(300);
 		});
 		$('.webform-client-form .form-text, .mail-list input[type="text"]').blur(function() {
 			var inputval = $(this).val();
 			if (inputval == "") {
-				$(this).parent().find('label:first').fadeIn();
+				$(this).parent().find('label:first').fadeIn(300);
 			}
 		});
 		
 		// Form Label Fades - Textarea
 		$('.webform-client-form textarea').focus(function() {
-			$(this).parent().parent().find('label:first').fadeOut();
+			$(this).parent().parent().find('label:first').fadeOut(300);
 		});
 		$('.webform-client-form textarea').blur(function() {
 			var inputval = $(this).val();
 			if (inputval == "") {
-				$(this).parent().parent().find('label:first').fadeIn();
+				$(this).parent().parent().find('label:first').fadeIn(300);
 			}
 		});
 		
